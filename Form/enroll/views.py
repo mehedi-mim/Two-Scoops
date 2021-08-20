1653eea62e5e46d18799868fde1ef5d8733fcece
Mehedi Hasan Mim <mehedi.hasan@vivacomslutions.com>
2021-08-20 17:38:32 +0600

    Form validation code

--- Form/enroll/views.py
+++ Form/enroll/views.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.shortcuts  import render
+from django.urls.base import is_valid_path
 from .forms import StudentRegistration
 # Create your models here.
 
 def showFormData(request):
-    fm = StudentRegistration()
+    if (request.method) == 'POST':
+        fm = StudentRegistration(request.POST)
+        if(fm.is_valid()):
+            print('From Validated')
+            print('Name:',fm.cleaned_data['name'])
+            print('Email:',fm.cleaned_data['email'])
+        else:
+            fm = StudentRegistration()
     return render(request,'enroll/userregistration.html',{'form':fm})
 
